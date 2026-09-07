428192406e8f6677abd48d80f82ffad7eebaf18e
thealing <159199097+thealing@users.noreply.github.com>
2026-09-07 21:43:21 +0200

    Updated time frame

--- Source/Library/Utils/Time.cpp
+++ Source/Library/Utils/Time.cpp
@@ -1,10 +1,34 @@
 #include "Time.h"
 
+TimeFrame* TimeFrame::getInstance()
+{
+	return &_instance;
+}
+
+LARGE_INTEGER TimeFrame::getFrequency() const
+{
+	return _frequency;
+}
+
+LARGE_INTEGER TimeFrame::getStartCounter() const
+{
+	return _startCounter;
+}
+
+TimeFrame::TimeFrame()
+{
+	QueryPerformanceFrequency(&_frequency);
+	QueryPerformanceCounter(&_startCounter);
+}
+
+TimeFrame TimeFrame::_instance;
+
 double getTime()
 {
 	LARGE_INTEGER counter;
 	QueryPerformanceCounter(&counter);
-	LARGE_INTEGER frequency;
-	QueryPerformanceFrequency(&frequency);
-	return (double)counter.QuadPart / (double)frequency.QuadPart;
+	TimeFrame* timeFrame = TimeFrame::getInstance();
+	LARGE_INTEGER frequency = timeFrame->getFrequency();
+	LARGE_INTEGER startCounter = timeFrame->getStartCounter();
+	return (double)(counter.QuadPart - startCounter.QuadPart) / (double)frequency.QuadPart;
 }
--- Source/Library/Utils/Time.h
+++ Source/Library/Utils/Time.h
@@ -1,3 +1,25 @@
 #pragma once
 
+struct TimeFrame
+{
+public:
+
+	static TimeFrame* getInstance();
+
+	LARGE_INTEGER getFrequency() const;
+
+	LARGE_INTEGER getStartCounter() const;
+
+private:
+
+	TimeFrame();
+
+private:
+
+	static TimeFrame _instance;
+
+	LARGE_INTEGER _frequency;
+	LARGE_INTEGER _startCounter;
+};
+
 double getTime();
